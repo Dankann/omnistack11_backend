3e4d1ecb3f8198e1c7af630a8da31d1d0f6bfb1c
Dankann <dankann.weissmuller@gmail.com>
2020-09-11 11:04:42 -0300

    Remove duplicate request

--- src/routes.js
+++ src/routes.js
@@ -7,17 +7,14 @@ const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 routes.post('/session', SessionController.create);
+routes.get('/profile', ProfileController.index);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
-routes.get('/profile', ProfileController.index);
-
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
-
-routes.get('/incidents', IncidentController.index);
 
 module.exports = routes;
 
