e9605fe089ae3e31eec30a87650a47fdfde61a3f
Dankann <dankann.weissmuller@gmail.com>
2020-09-18 08:10:33 -0300

    Change env to staging

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
-const env = process.env.NODE_ENV == 'test' ? configuration.test : configuration.development;
+const env = process.env.NODE_ENV == 'test' ? configuration.test : configuration.staging;
 
 const connection = knex(env);
 
